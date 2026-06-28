2abc5f0fa84b8ff206ec54626537ca8f3978a280
Manukonda Teja Harsha <tejamanukonda99@gmail.com>
2026-06-28 23:35:22 +0530

    Add files via upload

---
+++ Pizza_Delivery_Web/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  theme: {
+    extend: {
+      colors: {
+        domBlue: '#0a2f5b',
+        domRed: '#d1212a',
+        domYellow: '#f6af0a',
+      },
+    },
+  },
+  plugins: [],
+};
